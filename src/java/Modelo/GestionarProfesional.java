bb69c8baf0bd7e9d6cca89db52dfc880319bee03
Lautaro Pereyra <134530310+Lautaro027@users.noreply.github.com>
2024-10-04 08:20:34 -0300

    Añadimos el gestionar profesional

---
+++ src/java/Modelo/GestionarProfesional.java
@@ -0,0 +1,154 @@
+
+package Modelo;
+import Conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GestionarProfesional {
+    private Conexion c;
+    private Connection cn = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+   //Gestionar Profesional
+    public GestionarProfesional (){
+        this.c = new Conexion ();
+        this.cn = null;
+        this.ps = null;
+        this.rs = null;
+    }
+   //Código Insert
+    public boolean insertarProfesional (Profesional e) throws SQLException{
+        boolean respuesta = false;
+        try {
+            cn = c.conectar();
+            ps = cn.prepareStatement("INSERT INTO profesional (usuario, contrasena, nombre, apellido, telefono, email, fotoPerfil, tituloProfesional, calificacion, curriculum, direccion, localidad, provincia) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            ps.setString(1, e.getUsuario());
+            ps.setString(2, e.getContrasena());
+            ps.setString(3, e.getNombre());
+            ps.setString(4, e.getApellido());
+            ps.setInt(5, e.getTelefono());
+            ps.setString(6, e.getEmail());
+            ps.setString(7, e.getFotodePerfil());
+            ps.setString(8, e.getTitulodeProfesional());
+            ps.setInt(9, e.getCalificacion());
+            ps.setString(10, e.getCurriculum());
+            ps.setString(11, e.getDireccion());
+            ps.setString(12, e.getLocalidad());
+            ps.setString(13, e.getProvincia());
+            int res = ps.executeUpdate();
+            respuesta = res > 0;
+        } catch (SQLException exc) {
+           exc.printStackTrace();
+        } finally{
+            try {
+                if (rs!=null) {rs.close();}
+                if (ps!=null) {ps.close();}
+                if (cn!=null) {cn.close();}
+            } catch (SQLException exc2){
+               
+            }
+        }
+        return respuesta;
+    }
+   
+    //Buscar Profesional
+    public Profesional buscarProfesional(int id) throws SQLException{
+        Profesional e = new Profesional();
+        try {
+            cn = c.conectar();
+            ps = cn.prepareStatement("SELECT * FROM profesional WHERE id=?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()){
+                e.setId(rs.getInt(1));
+                e.setUsuario(rs.getString(2));
+                e.setContrasena(rs.getString(3));
+                e.setNombre(rs.getString(4));
+                e.setApellido(rs.getString(5));
+                e.setTelefono(rs.getInt(6));
+                e.setEmail(rs.getString(7));
+                e.setFotodePerfil(rs.getString(8));
+                e.setTitulodeProfesional(rs.getString(9));
+                e.setCalificacion(rs.getInt(10));
+                e.setCurriculum(rs.getString(11));
+                e.setDireccion(rs.getString(12));
+                e.setLocalidad(rs.getString(13));
+                e.setProvincia(rs.getString(14));
+            }
+        } catch (SQLException exc) {
+           
+        }finally{
+            try {
+                if (rs!=null) {rs.close();}
+                if (ps!=null) {ps.close();}
+                if (cn!=null) {cn.close();}
+            } catch (SQLException exc2){
+               
+            }
+        }
+        return e;
+    }
+   
+    //Eliminar
+    public boolean eliminarProfesional (int id) throws SQLException{
+        boolean respuesta = false;
+        try {
+            cn = c.conectar();
+            ps = cn.prepareStatement("DELETE FROM profesional WHERE id=?");
+            ps.setInt(1, id);
+            int res = ps.executeUpdate();
+            respuesta = res>0;
+        } catch (SQLException exc) {
+           
+        } finally{
+            try {
+                if (rs!=null) {rs.close();}
+                if (ps!=null) {ps.close();}
+                if (cn!=null) {cn.close();}
+            } catch (SQLException exc2){
+               
+            }
+        }
+        return respuesta;
+    }
+     //Modificar
+    public boolean modificarProfesional(Profesional e) throws SQLException {
+    boolean respuesta = false;
+    try {
+        cn = c.conectar();
+        ps = cn.prepareStatement("UPDATE profesional SET usuario = ?, contrasena = ?, nombre = ?, apellido = ?, telefono = ?, email = ?, fotoPerfil = ?, tituloProfesional = ?, calificacion = ?, curriculum = ?, direccion = ?, localidad = ?, provincia = ? WHERE id = ?");
+        ps.setString(1, e.getUsuario());
+        ps.setString(2, e.getContrasena());
+        ps.setString(3, e.getNombre());
+        ps.setString(4, e.getApellido());
+        ps.setInt(5, e.getTelefono());
+        ps.setString(6, e.getEmail());
+        ps.setString(7, e.getFotodePerfil());
+        ps.setString(8, e.getTitulodeProfesional());
+        ps.setInt(9, e.getCalificacion());
+        ps.setString(10, e.getCurriculum());
+        ps.setString(11, e.getDireccion());
+        ps.setString(12, e.getLocalidad());
+        ps.setString(13, e.getProvincia());
+        ps.setInt(14, e.getId());
+        int res = ps.executeUpdate();
+        respuesta = res > 0;
+    } catch (SQLException exc) {
+        exc.printStackTrace();
+    } finally {
+        try {
+            if (ps != null) {ps.close();}
+            if (cn != null) {cn.close();}
+        } catch (SQLException exc2) {
+            exc2.printStackTrace();
+        }
+    }
+    return respuesta;
+}
+   
+}
+
+
+    
