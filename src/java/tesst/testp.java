d22290657b5f20e993ab0ee445214ad395c681fc
Lautaro Pereyra <134530310+Lautaro027@users.noreply.github.com>
2024-10-04 08:51:47 -0300

    test del profesional

---
+++ src/java/tesst/testp.java
@@ -0,0 +1,27 @@
+
+package tesst;
+
+import Modelo.GestionarProfesional;
+import Modelo.Profesional;
+import java.sql.SQLException;
+
+
+public class testp {
+    public static void main (String[] args) throws SQLException{
+    //prueba del buscar profesional
+    GestionarProfesional a = new GestionarProfesional();
+    Profesional p = new Profesional();
+    p =  a.buscarProfesional(2);
+    System.out.print (p.getNombre());
+    //prueba del insertar
+    Profesional p1 = new Profesional("p1", "123", "juan", "perez", 123456789, "email@gmail.com", "foto.jpg", "Ingeniero", 5, "curriculum.pdf", "avenidaj 123", "Localidad", "Provincia");
+        System.out.println(a.insertarProfesional(p1));
+        //prueba del eliminar
+        boolean eliminado = a.eliminarProfesional(5); 
+        System.out.println("Profesional eliminado: " + eliminado);
+        //prueba del modificar
+            p1.setId(3);
+            boolean modificado = a.modificarProfesional(p1);
+            System.out.println("Profesional modificado: " + modificado);
+    }
+}
