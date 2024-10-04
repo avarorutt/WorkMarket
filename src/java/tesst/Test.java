d86d47abe63f80ecb2bb3755a209c1adac33e7e4
Lautaro Pereyra <134530310+Lautaro027@users.noreply.github.com>
2024-10-04 08:48:02 -0300

    añadimos el test de publicacionE

--- src/java/tesst/Test.java
+++ src/java/tesst/Test.java
@@ -1,27 +1,26 @@
+package test;
 
-package tesst;
-
-import Modelo.GestionarProfesional;
-import Modelo.Profesional;
+import Modelo.GestionarPublicacionE;
+import Modelo.PublicacionE;
 import java.sql.SQLException;
 
 
 public class Test {
     public static void main (String[] args) throws SQLException{
     //prueba del buscar profesional
-    GestionarProfesional a = new GestionarProfesional();
-    Profesional p = new Profesional();
-    p =  a.buscarProfesional(2);
-    System.out.print (p.getNombre());
+    GestionarPublicacionE a = new GestionarPublicacionE();
+    PublicacionE p = new PublicacionE();
+    p =  a.buscarPublicacionId(2);
+    System.out.print (p.getTitulo());
     //prueba del insertar
-    Profesional p1 = new Profesional("p1", "123", "juan", "perez", 123456789, "email@gmail.com", "foto.jpg", "Ingeniero", 5, "curriculum.pdf", "avenidaj 123", "Localidad", "Provincia");
-        System.out.println(a.insertarProfesional(p1));
-        //prueba del eliminar
-        boolean eliminado = a.eliminarProfesional(5); 
-        System.out.println("Profesional eliminado: " + eliminado);
+    PublicacionE p1 = new PublicacionE(5,"Programa de Voluntariado", "Únete a nuestro equipo para ayudar en comunidades vulnerables. Haz la diferencia.", "Disponibilidad de al menos 5 horas semanales, ser mayor de 16 años, buen trato con personas.",1);
+           System.out.println(a.insertarPublicacionE(p1));
+ //prueba del eliminar
+        boolean eliminado = a.eliminarPublicacionE(4);
+        System.out.println("Publicacion eliminada: " + eliminado);
         //prueba del modificar
             p1.setId(3);
-            boolean modificado = a.modificarProfesional(p1);
-            System.out.println("Profesional modificado: " + modificado);
+            boolean modificado = a.modificarPublicacionE(p1);
+            System.out.println("Publicacion modificada: " + modificado);
     }
 }
